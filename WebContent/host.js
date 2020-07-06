dcb9afb04e87bfdff800987c6118d8da0ade7cb5
Jovan Bosnic <jovan.bosnic_7@uns.ac.rs>
2020-07-06 19:00:33 +0200

    #sortApartments Implementirano je sortiranje apartmana po ceni za host-a i administratora

--- WebContent/host.js
+++ WebContent/host.js
@@ -1371,7 +1371,17 @@ $(document).on("click", "a.acceptReservationLink", function(){
 					}    
 			 }
 		});
-    });
+	});
+	$('#sortApartments').click(function(event) {
+			
+		sortTable();
+
+	});
+	$('#sortApartmentsDescending').click(function(event) {
+		
+		sortTableDescending();
+
+	});
     $('#openComments').click(function(){
         $('#showUsers').hide();
         $('#showApartments').hide();
@@ -1513,4 +1523,72 @@ function addApartment(apartment){
     tr.append(image).append(name).append(roomNumber).append(guestNumber).append(location).append(apartmentType).append(price).append(host).append(brisanje).append(izmena);
      $('#tableApartments tbody').append(tr);
 }
+function sortTable() {
+	var table, rows, switching, i, x, y, shouldSwitch;
+	table = document.getElementById("tableApartments");
+	switching = true;
+	/*Make a loop that will continue until
+	no switching has been done:*/
+	while (switching) {
+	  //start by saying: no switching is done:
+	  switching = false;
+	  rows = table.rows;
+	  /*Loop through all table rows (except the
+	  first, which contains table headers):*/
+	  for (i = 1; i < (rows.length - 1); i++) {
+		//start by saying there should be no switching:
+		shouldSwitch = false;
+		/*Get the two elements you want to compare,
+		one from current row and one from the next:*/
+		x = rows[i].getElementsByTagName("td")[6];
+		y = rows[i + 1].getElementsByTagName("td")[6];
+		//check if the two rows should switch place:
+		if (Number(x.innerHTML) > Number(y.innerHTML)) {
+		  //if so, mark as a switch and break the loop:
+		  shouldSwitch = true;
+		  break;
+		}
+	  }
+	  if (shouldSwitch) {
+		/*If a switch has been marked, make the switch
+		and mark that a switch has been done:*/
+		rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+		switching = true;
+	  }
+	}
+  }
+  function sortTableDescending() {
+	var table, rows, switching, i, x, y, shouldSwitch;
+	table = document.getElementById("tableApartments");
+	switching = true;
+	/*Make a loop that will continue until
+	no switching has been done:*/
+	while (switching) {
+	  //start by saying: no switching is done:
+	  switching = false;
+	  rows = table.rows;
+	  /*Loop through all table rows (except the
+	  first, which contains table headers):*/
+	  for (i = 1; i < (rows.length - 1); i++) {
+		//start by saying there should be no switching:
+		shouldSwitch = false;
+		/*Get the two elements you want to compare,
+		one from current row and one from the next:*/
+		x = rows[i].getElementsByTagName("td")[6];
+		y = rows[i + 1].getElementsByTagName("td")[6];
+		//check if the two rows should switch place:
+		if (Number(x.innerHTML) < Number(y.innerHTML)) {
+		  //if so, mark as a switch and break the loop:
+		  shouldSwitch = true;
+		  break;
+		}
+	  }
+	  if (shouldSwitch) {
+		/*If a switch has been marked, make the switch
+		and mark that a switch has been done:*/
+		rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+		switching = true;
+	  }
+	}
+  }
 
